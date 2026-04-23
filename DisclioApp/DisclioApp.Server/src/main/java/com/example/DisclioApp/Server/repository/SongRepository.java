2026de8e555069a2fae472e3b06e6c8b56edd95f
diana-reut <dianaradauti2013@gmail.com>
2026-04-23 08:45:22 +0300

    added songs + all CRUD operations for them:)

---
+++ DisclioApp/DisclioApp.Server/src/main/java/com/example/DisclioApp/Server/repository/SongRepository.java
@@ -0,0 +1,4 @@
+package com.example.DisclioApp.Server.repository;
+
+public class SongRepository {
+}
---
+++ DisclioApp/DisclioApp.Server/src/main/java/com/example/DisclioApp/Server/service/SongService.java
@@ -0,0 +1,4 @@
+package com.example.DisclioApp.Server.service;
+
+public class SongService {
+}
